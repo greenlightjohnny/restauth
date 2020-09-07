8244e709422209a8dae68a41aaeabfaa306d1c5c
fj <castelloiv@gmail.com>
2020-09-07 14:41:25 -0400

    Added Joi for validation, sweet

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
 const router = require("express").Router();
+const User = require("../model/User");
+const Joi = require(`@hapi/joi`);
 
-router.post("/register", (req, res) => {
-  res.send(
-    "Register, displays when the /register path is hit with an http post method"
-  );
+//Validation!!! YEES
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+router.post("/register", async (req, res) => {
+  ///Validate data submitted before sending to MongoDB
+  const validation = schema.validate(req.body);
+
+  res.send(validation);
+  console.log(validation);
+
+  //   const user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //   });
+  //   try {
+  //     const savedUser = await user.save();
+  //     console.log('auth saved user', savedUser)
+  //     res.send(savedUser);
+  //   } catch (err) {
+  //     res.status(400).send(err);
+  //   }
 });
 
 module.exports = router;
