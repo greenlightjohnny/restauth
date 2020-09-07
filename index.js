3cc3cf0c574a46bd591f7a1ad9b81914fa30dee2
fj <castelloiv@gmail.com>
2020-09-07 13:51:16 -0400

    auth module

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+////Import Routes
+const authRoute = require("./routes/auth");
+
 ///Connect to database with Mongoose
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PWORD}@cluster0.1hf7x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
@@ -10,9 +13,10 @@ mongoose.connect(
   () => console.log("Connected to the MongoDB")
 );
 
-////Import Routes
-const authRoute = require("./routes/auth");
+///Middleware
+app.use(express.json());
 
+///Route Middlewares
 app.use("/api/user", authRoute);
 
 app.listen(3000, () => console.log("Server is UP!"));
